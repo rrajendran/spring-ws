760b5291335126f0cb48ba7481543d9f3508be6a
rrajendran <rameshchinnu61@gmail.com>
2013-03-01 11:39:49 +0000

    Deleted files

--- spring-ws-service/src/main/java/com/capella/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capella.users;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends CrudRepository<User, Long> {
-  User findByUsernameAndPassword(String username, String password);
-  User findByUsername(String username);
-}
